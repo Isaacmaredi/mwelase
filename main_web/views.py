01ed2ac8263559e8b8d019f8df176b0f15ca845c
isaacmaredi <isaacmaredi@icloud.com>
2021-04-18 18:40:07 +0200

    Disabled email send

--- main_web/views.py
+++ main_web/views.py
@@ -11,11 +11,11 @@ from main_app.models import Project, Gallery
 
 def index(request):
     projects = Project.objects.all().order_by('-start_date')[:3]
-    
+
     context = {
         'projects':projects,
     }
-    
+
     return render(request,'main_web/index.html',context)
 
 def about(request):
@@ -32,18 +32,18 @@ def contact(request):
         form = ContactForm()
     else:
         form = ContactForm(request.POST)
-        if form.is_valid():       
+        if form.is_valid():
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             subject = form.cleaned_data['subject']
             from_email = form.cleaned_data['from_email']
             website= form.cleaned_data['website']
             message = form.cleaned_data['message']
-            try:
-                send_mail(subject, from_email,message, ['isaac.maredi@gmail.com',from_email])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
+            form.save()
+            # try:
+            #     send_mail(subject, from_email,message, ['isaac.maredi@gmail.com',from_email])
+            # except BadHeaderError:
+            #     return HttpResponse('Invalid header found.')
             return redirect('index')
-            
-    return render(request, "main_web/contact.html", {'form': form})
 
+    return render(request, "main_web/contact.html", {'form': form})
